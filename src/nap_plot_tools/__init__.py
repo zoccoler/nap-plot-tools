fad0d1c51d9cd517102b9d5fdde028f72190638c
Marcelo Zoccoler <marcelozoc@gmail.com>
2023-11-28 15:11:55 +0100

    bump version and add new cmap function

--- src/nap_plot_tools/__init__.py
+++ src/nap_plot_tools/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 from .tools import QtColorSpinBox, CustomToolbarWidget
-from .cmap import get_custom_cat10based_cmap_list
+from .cmap import make_cat10_mod_cmap, get_custom_cat10based_cmap_list # get_custom_cat10based_cmap_list is deprecated
