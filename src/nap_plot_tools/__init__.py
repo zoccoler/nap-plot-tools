dc7eadc7f7d671eaba0287625e1f118e278df366
Marcelo Zoccoler <marcelozoc@gmail.com>
2023-11-28 15:40:28 +0100

    set alpha version

--- src/nap_plot_tools/__init__.py
+++ src/nap_plot_tools/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.3"
+__version__ = "0.0.3-alpha"
 
 from .tools import QtColorSpinBox, CustomToolbarWidget
 from .cmap import make_cat10_mod_cmap, get_custom_cat10based_cmap_list # get_custom_cat10based_cmap_list is deprecated
