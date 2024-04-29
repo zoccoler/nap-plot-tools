5620523bc5a7c56dac2d0284b66d2c28715bed0e
Marcelo Zoccoler <marcelozoc@gmail.com>
2024-04-29 10:28:51 +0200

    bump version and update available colormaps

--- src/nap_plot_tools/__init__.py
+++ src/nap_plot_tools/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.4"
+__version__ = "0.1.0"
 
 from .tools import QtColorSpinBox, CustomToolbarWidget
-from .cmap import cat10_mod_cmap, cat10_mod_cmap_first_opaque, make_cat10_mod_cmap, get_custom_cat10based_cmap_list # get_custom_cat10based_cmap_list is deprecated
+from .cmap import cat10_mod_cmap, cat10_mod_cmap_first_transparent, make_cat10_mod_cmap, get_custom_cat10based_cmap_list # get_custom_cat10based_cmap_list is deprecated
